24b962343ef003fa6ad30960b43267f4c1df3433
KKaustubh <kksitar@gmail.com>
2024-12-29 19:26:57 +0530

    Add files via upload

---
+++ Pr16.py
@@ -0,0 +1,44 @@
+# Calendar used to print names
+# Easy method- print("January,February,March,April-------")
+import calendar
+import time
+yy=2024
+m1=1
+m2=2
+m3=3
+m4=4
+m5=5
+m6=6
+m7=7
+m8=8
+m9=9
+m10=10
+m11=11
+m12=12
+from time import sleep;
+sleep(1)
+print(calendar.month(yy,m1))
+sleep(1)
+print(calendar.month(yy,m2))
+sleep(1)
+print(calendar.month(yy,m3))
+sleep(1)
+print(calendar.month(yy,m4))
+sleep(1)
+print(calendar.month(yy,m5))
+sleep(1)
+print(calendar.month(yy,m6))
+sleep(1)
+print(calendar.month(yy,m7))
+sleep(1)
+print(calendar.month(yy,m8))
+sleep(1)
+print(calendar.month(yy,m9))
+sleep(1)
+print(calendar.month(yy,m10))
+sleep(1)
+print(calendar.month(yy,m11))
+sleep(1)
+print(calendar.month(yy,m12))
+sleep(1)
+print("Thanks for waiting this long XD")
